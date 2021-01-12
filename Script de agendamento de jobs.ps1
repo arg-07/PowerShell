a34e4e8d8f4caaab9be31a277c5e53f51163de05
Gustavo Barros <77308865+arg-07@users.noreply.github.com>
2021-01-11 23:13:00 -0300

    Script de agendamento de jobs
    Script para powershell que cria jobs agendados

---
+++ Script de agendamento de jobs.ps1
@@ -0,0 +1,16 @@
+﻿#Script de agendamento de jobs
+
+Clear-Host
+
+Get-Command -Module PSScheduledJob | Sort-Object Noum
+
+$diario = New-JobTrigger -Daily -At 3am
+$umavez = New-JobTrigger -Once -At (Get-Date).AddHours(1)
+$semanal = New-JobTrigger -Weekly -DaysOfWeek Monday -At 6pm
+
+Register-ScheduledJob -Name Backup -Trigger $diario -ScriptBlock 
+{
+Copy-Item C:\Users\*.* C:\concurso -Recurse -Force
+}
+
+Get-ScheduledJob Backup
